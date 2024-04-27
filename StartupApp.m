7b909393de2676a353c35f0a4f061044385e98da
Jasprabhjit Mehami <jasprabhjit.mehami@student.uts.edu.au>
2024-04-27 20:03:06 +1000

    separate app to compare two hypercubes

--- StartupApp.m
+++ StartupApp.m
@@ -1,5 +1,24 @@
 close all;
 clear;
 
-load("dataset/reflectance_hypercube_lamb_cut.mat", 'fatDepthCubeY', 'reflectanceCubeX');
-runapp = reflectance_fatdepth_viewer(reflectanceCubeX, fatDepthCubeY);
+
+datasetPath = "/data/jamehami/lamb_carcass_une_2021/fat_depth_regression/saddle/hypercube-wise/calibrated_reflectance.mat";
+load(datasetPath, 'fatDepthCubeY', 'reflectanceCubeX');
+scalar1 = fatDepthCubeY;
+cube1 = reflectanceCubeX;
+
+datasetPath = "/data/jamehami/lamb_carcass_une_2022/fat_depth_regression/saddle_100/hypercube-wise/calibrated_reflectance.mat";
+% datasetPath = "/data/jmehami/lamb_carcass_une_2021/fat_depth_regression/leg_left_half/hypercube-wise/estimated_reflectance_mehami.mat";
+load(datasetPath, 'fatDepthCubeY', 'reflectanceCubeX');
+scalar2 = fatDepthCubeY;
+cube2 = reflectanceCubeX;
+
+scaler_hypercube_comparison(cube1, scalar1, cube2, scalar2);
+
+% runapp1 = reflectance_fatdepth_viewer(reflectanceCubeX, fatDepthCubeY);
+% 
+% datasetPath = "/data/jmehami/lamb_carcass_une_2021/fat_depth_regression/leg_left/hypercube-wise/estimated_reflectance_mehami.mat";
+% load(datasetPath, 'fatDepthCubeY', 'reflectanceCubeX');
+% runapp = reflectance_fatdepth_viewer(reflectanceCubeX, fatDepthCubeY);
+
+%% add legenddddddddd
